5127373d8088c74f7086f5b26b2621400b949ce1
Thanh Khac <kingkid162@gmail.com>
2016-12-13 12:53:40 +0700

    add sessions

---
+++ models/patients.js
@@ -0,0 +1,11 @@
+/**
+ * Created by NarX on 12/13/16.
+ */
+
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+var PatientSchema = mongoose.Schema({
+
+});
---
+++ routes/patients.js
@@ -0,0 +1,8 @@
+/**
+ * Created by NarX on 12/13/16.
+ */
+var express = require('express');
+var router = express.Route();
+
+var User = require('../models/users');
+var Patients = require('../models/patients');
